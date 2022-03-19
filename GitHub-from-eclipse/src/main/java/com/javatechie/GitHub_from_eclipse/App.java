07d23e5b2c657e4d1077a95fcb4ad5144a6ee381
kimlong89 <Kim-Long Le@lovingdevice.attlocal.net>
2022-03-19 11:05:48 -0700

    Second Commit with Changes

--- GitHub-from-eclipse/src/main/java/com/javatechie/GitHub_from_eclipse/App.java
+++ GitHub-from-eclipse/src/main/java/com/javatechie/GitHub_from_eclipse/App.java
@@ -1,13 +1,23 @@
 package com.javatechie.GitHub_from_eclipse;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App{
+		public static void main(String[] args) {
+	//	System.out.println("Hello World");
+		 try{
+	         int a[]=new int[7];
+	         a[4]=30/0;
+	         System.out.println("First print statement in try block");
+	     }
+	     catch(ArithmeticException e){
+	        System.out.println("Warning: ArithmeticException");
+	     }
+	     catch(ArrayIndexOutOfBoundsException e){
+	        System.out.println("Warning: ArrayIndexOutOfBoundsException");
+	     }
+	     catch(Exception e){
+	        System.out.println("Warning: Some Other exception");
+	     }
+	   System.out.println("Out of try-catch block...");
+   
+	   //test
+	}
 }
